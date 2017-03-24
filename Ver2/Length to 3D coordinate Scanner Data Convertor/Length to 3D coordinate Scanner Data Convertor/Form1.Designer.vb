22b4266652c207405f4cfe26020951e557492b8b
Toby Chui <tobychui@users.noreply.github.com>
2017-03-24 15:48:21 +0800

    Software for Scan Data to Point Cloud

---
+++ Ver2/Length to 3D coordinate Scanner Data Convertor/Length to 3D coordinate Scanner Data Convertor/Form1.Designer.vb
@@ -0,0 +1,77 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox1.Size = New System.Drawing.Size(190, 615)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = resources.GetString("TextBox1.Text")
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(321, 12)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox2.Size = New System.Drawing.Size(193, 615)
+        Me.TextBox2.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(208, 310)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(107, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Convert"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(526, 639)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "L23D Convertor"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Ver2/Length to 3D coordinate Scanner Data Convertor/Length to 3D coordinate Scanner Data Convertor/Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+    Dim data() As String
+    Dim tempspliter() As String
+    Dim x As Double
+    Dim y As Double
+    Dim z As Double
+    Dim r As Double 'radius
+    Dim p As Double 'angle from center
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        On Error Resume Next
+        data = TextBox1.Text.Split(vbNewLine)
+        Dim rawdata As String
+        For Each rawdata In data
+            tempspliter = rawdata.Split(" ")
+            'Put the right value into the right place
+            x = tempspliter(0)
+            y = tempspliter(1)
+            z = tempspliter(2)
+            'Data Analysis and sorting
+            z = z / 22 'Convert z from steps to cm
+            r = 65 - x 'Radius from center of rotating platform to object edge
+            p = y / 2048 * 360 ' Convert angle from step to degree
+            x = r * Math.Cos(p)
+            y = r * Math.Sin(p)
+            TextBox2.AppendText(x & " " & y & " " & z & vbNewLine)
+        Next
+    End Sub
+End Class
